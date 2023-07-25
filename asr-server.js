f911210e6b7189af09e971912c32bbaf0512b254
Ryan Schwers <schwers.r@gmail.com>
2023-07-25 12:34:47 -0700

    Add readme. improve mocks a little

--- asr-server.js
+++ asr-server.js
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import { TranscriptMocks } from "./mock-transcripts.js";
 
 const DELAY_MS = 5_000;
 const FAILURE_RATE = 1 / 5;
@@ -7,25 +8,6 @@ const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const addRandomRequestLatency = async () => {
   await sleep(DELAY_MS + Math.random() * DELAY_MS);
 };
-
-const TranscriptMocks = new Map();
-const addMock = ({ path, text, shouldError = false }) => {
-  TranscriptMocks.set(path, { text, shouldError });
-};
-
-addMock({ path: "audio-file-1.wav", text: "This is the first audio file" });
-addMock({ path: "audio-file-2.wav", text: "This is the second audio file" });
-addMock({ path: "audio-file-3.wav", text: "This is the third audio file" });
-addMock({ path: "audio-file-4.wav", text: "This is the fourth audio file" });
-addMock({ path: "audio-file-5.wav", text: "This is the fifth audio file" });
-addMock({ path: "audio-file-6.wav", text: "This is the sixth audio file" });
-addMock({ path: "audio-file-7.wav", text: "This is the seventh audio file" });
-addMock({ path: "audio-file-8.wav", text: "This is the ninth audio file" });
-addMock({
-  path: "audio-file-9.wav",
-  text: "This is the eigth audio file",
-  shouldError: true,
-});
 
 const fastify = Fastify({
   logger: true,
@@ -49,6 +31,8 @@ fastify.get("/get-asr-output", async function handler(request, reply) {
 });
 
 try {
+  console.log("Starting server..., supported paths:");
+  console.log(TranscriptMocks.keys());
   await fastify.listen({ port: 3000 });
 } catch (err) {
   fastify.log.error(err);
